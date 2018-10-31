f49b2fdd0122620dd3e8689b0f9eac7aa0146cda
Amarius1 <amarius2000@gmail.com>
2018-10-31 21:13:44 +0200

    Add files via upload

--- logic.js
+++ logic.js
@@ -6,3 +6,20 @@ document.addEventListener("DOMContentLoaded", function(){
 		.fadeOut();
 });
 AOS.init()
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene);
+//$("#container").mousemove(function(e) {
+  parallaxIt(e, "#title", -30);
+  parallaxIt(e, "#buttons", -40);
+});
+
+function parallaxIt(e, target, movement) {
+  var $this = $("#container");
+  var relX = e.pageX - $this.offset().left;
+  var relY = e.pageY - $this.offset().top;
+
+  TweenMax.to(target, 1, {
+    x: (relX - $this.width() / 2) / $this.width() * movement,
+    y: (relY - $this.height() / 2) / $this.height() * movement
+  });
+}
